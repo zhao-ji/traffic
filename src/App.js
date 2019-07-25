c345ad2bb0291ed8e605939fabca5f593899edeb
Trevor.Max <Trevor.Max@btg.co.nz>
2019-07-26 11:27:39 +1200

    Issue #master - add react-bootstrap and react-router into project, starts with navigation bar

--- src/App.js
+++ src/App.js
@@ -1,26 +1,52 @@
 import React from 'react';
-import logo from './logo.svg';
+import {HashRouter as Router, Switch, Route, Link} from "react-router-dom";
+import { Navbar, Nav } from "react-bootstrap";
+
 import './App.css';
+import Home from './components/home';
+import Trace from './components/trace';
+import Edit from './components/edit';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Router>
+          <div>
+              <Header />
+              <hr />
+              <Switch>
+                  <Route exact path="/" component={Home} />
+                  <Route path="/trace" component={Trace} />
+                  <Route path="/edit" component={Edit} />
+                  <Route path="/about" render={() => <h3>about this project</h3>} />
+                  <Route render={() => <h3>No Match</h3> } />
+              </Switch>
+          </div>
+      </Router>
   );
+}
+
+function Header() {
+    return (
+        <Navbar bg="light" expand="lg">
+            <Navbar.Brand>
+                <Link to="/"> Traffic Monitor </Link>
+            </Navbar.Brand>
+            <Nav justify variant="pills">
+                <Nav.Item>
+                    <Link to="/">Home</Link>
+                </Nav.Item>
+                <Nav.Item>
+                    <Link to="/trace">Trace Route</Link>
+                </Nav.Item>
+                <Nav.Item>
+                    <Link to="/edit">Edit Point and Route</Link>
+                </Nav.Item>
+                <Nav.Item>
+                    <Link to="/about">About</Link>
+                </Nav.Item>
+            </Nav>
+        </Navbar>
+    );
 }
 
 export default App;
---
+++ src/components/trace.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function Trace() {
+    return (
+        <div>
+            <h2>
+                Trace
+            </h2>
+        </div>
+    );
+}
