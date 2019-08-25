6a4fe0bc6226236c82f146543f08ba8aa5adbaef
Subs <BytesWarning@gmail.com>
2019-08-26 00:06:31 +1200

    Issue #master - add edit page line items

--- src/components/edit.js
+++ src/components/edit.js
@@ -28,6 +28,41 @@ export default class extends Component {
         }
     }
 
+    renderTrace() {
+        return (
+            <>
+            <hr />
+            <Row>
+                <Col md={{ span: 8, offset: 2}} lg={{ span: 8, offset: 2}}>
+                    <h3>
+                        Trace
+                    </h3>
+                    <Table striped bordered hover>
+                        <thead>
+                            <tr>
+                                <th>ID</th>
+                                <th>Route</th>
+                                <th>Duration</th>
+                                <th>Distance</th>
+                                <th>Time</th>
+                                <th>Operate</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {
+                                !this.props.edit.trace.results.isLoading &&
+                                this.props.edit.trace.results.map(
+                                    (result, index) => <TraceLineItem key={index} result={result} />
+                                )
+                            }
+                        </tbody>
+                    </Table>
+                </Col>
+            </Row>
+            </>
+        );
+    }
+
     render() {
         return (
             <>
@@ -48,6 +83,12 @@ export default class extends Component {
                             </tr>
                         </thead>
                         <tbody>
+                            {
+                                !this.props.edit.address.results.isLoading &&
+                                this.props.edit.address.results.map(
+                                    (result, index) => <AddressLineItem key={index} result={result} />
+                                )
+                            }
                         </tbody>
                     </Table>
                 </Col>
@@ -70,11 +111,98 @@ export default class extends Component {
                             </tr>
                         </thead>
                         <tbody>
+                            {
+                                !this.props.edit.route.results.isLoading &&
+                                this.props.edit.route.results.map(
+                                    (result, index) =>
+                                    <RouteLineItem
+                                        key={index} result={result}
+                                        fetchTrace={this.props.fetchTrace}
+                                    />
+                                )
+                            }
                         </tbody>
                     </Table>
                 </Col>
             </Row>
+            {this.renderTrace()}
             </>
         );
     }
 }
+
+
+class AddressLineItem extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+        };
+    }
+
+    render() {
+        return (
+            <tr>
+                <td>{this.props.result.id}</td>
+                <td>{this.props.result.address}</td>
+                <td>{this.props.result.alias}</td>
+                <td>{this.props.result.latitude}</td>
+                <td>{this.props.result.longitude}</td>
+                <td>
+                    <button>Delete</button>
+                    <button>Related Route</button>
+                </td>
+            </tr>
+        );
+    }
+}
+
+
+class RouteLineItem extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+        };
+    }
+
+    render() {
+        return (
+            <tr>
+                <td>{this.props.result.id}</td>
+                <td>{this.props.result.start.alias || this.props.result.start.address}</td>
+                <td>{this.props.result.stop.alias || this.props.result.stop.address}</td>
+                <td>{this.props.result.method}</td>
+                <td>{this.props.result.cron}</td>
+                <td>
+                    <button>Delete</button>
+                    <button onClick={() => this.props.fetchTrace({route_id: this.props.result.id})}>
+                        Related Trace
+                    </button>
+                </td>
+            </tr>
+        );
+    }
+}
+
+
+class TraceLineItem extends Component {
+     constructor(props) {
+         super(props);
+         this.state = {
+         };
+     }
+ 
+     render() {
+         return (
+             <tr>
+                 <td>{this.props.result.id}</td>
+                 <td>{this.props.result.route.id}</td>
+                 <td>{(this.props.result.duration/60).toFixed(2)} min</td>
+                 <td>{(this.props.result.distance/1000).toFixed(2)} km</td>
+                 <td>{this.props.result.created_at}</td>
+                 <td>
+                     <button>Delete</button>
+                 </td>
+             </tr>
+         );
+     }
+ }
